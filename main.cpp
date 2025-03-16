c40dc111614666fbc515816e61632c98c9fe826d
ducvnkm2k4 <duckmhd2k4@gmail.com>
2025-03-16 23:27:35 +0700

    ..

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
-
+#include <thread>
 #include "protocol.h"
 #include "network.h"
+void menu()
+{
+    std::cout << "----------------------MENU-------------------" << std::endl;
+    std::cout << "1.send ping" << std::endl;
 
+    std::cout << "choose?:";
+}
+void choose()
+{
+    while (true)
+    {
+        menu();
+        int choose;
+        std::cin >> choose;
+
+        switch (choose)
+        {
+        case 1:
+            sendPingMessage("192.168.12.2", portUDP);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
 int main()
 {
     loadConfig();
-    listenConnectTCP(portTCP);
-    listenMessageUDP(portUDP);
+    std::thread connecttcp(sendMessageTCP, "192.168.12.2", 50001);
+    connecttcp.detach();
+    std::thread udpListener(listenMessageUDP, portUDP);
+    udpListener.detach();
+
+    choose();
     return 0;
 }
